394496c9b42bb1efcd2b208259d4666039f8ee93
Chia-hao Chang <Charles@Chia-haos-MacBook-Pro.local>
2017-07-05 13:33:44 +0800

    resolved all google-map-react dependencies; make sure environment runs

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -3,16 +3,23 @@
 var React = require('react');
 var Router = require('react-router');
 var Link = Router.Link; 
-// var Editor = require('./editor');
-
+var gMap = require('google-map-react');
 
 var Home = React.createClass({
+    initMap: function() {
+        var map = new gMap.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+    },
+
 	render: function() {
 		return (
 			<div className="jumbotron">
 				<h1>Welcome to Learning Home Page</h1>
 				<p>React, React Router, Flux for ultra-responsive webapp.</p>
 				<Link to="about" className="btn btn-primary btn-lg">Learn more...</Link>
+				<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCS40sFe_ZSVXlNyXzaAt5x56VoI7HiJy4&callback=initMap" async defer></script>
 			</div>
 		);
 	}
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 "use strict"; 
 
 var React = require('react');
+var ReactDOM = require('react-dom');
 var Router = require('react-router');
 var routes = require('./routes'); 
 
 Router.run(routes, Router.HistoryLocation, function(Handler) {
 //Router.run(routes, function(Handler) {
-	React.render(<Handler/>, document.getElementById('app'));
+	ReactDOM.render(<Handler/>, document.getElementById('app'));
 }); 
